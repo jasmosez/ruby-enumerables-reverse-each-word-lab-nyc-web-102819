269ff27fd7454c4a4dc399d23b3a142e384a3b33
James Schaffer <jas.schaffer@gmail.com>
2019-09-17 15:02:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,9 @@
 def reverse_each_word(sentence)
+  
   sentence_as_array = sentence.split
-  p sentence_as_array
-  sentence_as_array.each
+  new = []
+  sentence_as_array.each do
+    |word|
+    word.reverse
+  end  
 end
